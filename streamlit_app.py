8817e789d76d5119565404d18c275bcf458f1e9f
Vinipurcinelli-hub <vi.meira2010@gmail.com>
2025-07-22 14:02:54 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -245,16 +245,14 @@ for cod in sorted(LEGENDA_OBS.keys()):
             x=[None],
             y=[None],
             mode="markers",
-            marker=dict(size=10, color="white", symbol="circle"),
+            marker=dict(size=10, color="white", symbol="circle"),  # cor neutra
             showlegend=True,
             name=texto,
             hoverinfo="skip",
             legendgroup="OBS",
-            legendrank=cod + 100,  # 100+ para aparecer após as cores
             xaxis="x2"
         )
     )
-
 
 # Layout final
 fig.update_layout(
