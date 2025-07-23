5851842a901d1ba619d9e8393fdd8724e545d0ed
Vinipurcinelli-hub <vi.meira2010@gmail.com>
2025-07-23 16:28:51 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -299,7 +299,7 @@ for i, x in enumerate(ticks_dias):
         yref="paper",
         text=f"<b>{dias_semana[i]}</b>",
         showarrow=False,
-        font=dict(size=14, color="black"),
+        font=dict(size=14, color="white"),
         align="center"
     ))
 
@@ -333,7 +333,7 @@ for cod in sorted(LEGENDA_OBS.keys(), reverse=True):
             x=[None],
             y=[None],
             mode="markers",
-            marker=dict(size=10, color="black", symbol="circle"),  # cor neutra
+            marker=dict(size=10, color="white", symbol="circle"),  # cor neutra
             showlegend=True,
             name=texto,
             hoverinfo="skip",
